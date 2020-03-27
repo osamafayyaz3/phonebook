b40edb4ea50096fbbe3d124a1a2b0f51cdf6fccd
osamafayyaz3 <osama85@my.yorku.ca>
2020-03-26 22:22:08 -0400

    v2.6

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Directory = ({dir}) => {
+  return (
+    dir.map(dirs => <div>{dirs.name}</div>)
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    {name: 'Arto Hellas'}
+  ])
+
+  const [newName, setNewName] = useState('')
+
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+        name: newName
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Directory dir={persons}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
